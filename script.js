a972345309db1adb2f7d5f27afc008309f01dc06
Misheline <71193430+Misheline@users.noreply.github.com>
2021-03-12 23:36:02 -0500

    redid homework

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-function clearOutput() {
-    document.getElementById("output").innerHTML = "0";
+function result(){
+    var Number1 = Number(document.getElementById("Number1").value);
+    var Number2 = Number(document.getElementById("Number2").value);
+
+    document.getElementById("result").value = Number1 + Number2
 }
-function removeZero() {
-    var value = document.getElementById("output").innerHTML;
-    if (value == "0") {
-         value = " "
-         document.getElementById("output").innerHTML = value;
-    }
-}
-function fordisplay(myvalue) {
-    removeZero()
-    document.getElementById("output").innerHTML += myvalue;
-}
-function solve() {
-    removeZero()
-    var equation = document.getElementById("output").innerHTML;
-    var solved = eval(equation);
-    document.getElementById('output').innerHTML = solved;
-}
+    
