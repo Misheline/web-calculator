578cdbe3adc4ac066caf2cfd6ac90a058e57d4fa
Misheline <71193430+Misheline@users.noreply.github.com>
2021-03-13 21:48:20 -0500

    Part 2

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function solve() {
     var solved = eval(equation);
     document.getElementById('output').innerHTML = solved;
 }
+
+function result() {
+    var Number1 = Number(document.getElementById("Number1").value);
+    var Number2 = Number(document.getElementById("Number2").value);
+    
+    switch (document.getElementById("operator").value)
+    {
+        case "addition":
+        document.getElementById("result").value = Number1 + Number2;
+        break;
+
+        case "subtraction":
+        document.getElementById("result").value = Number1 - Number2;
+        break;
+
+        case "multi":
+        document.getElementById("result").value = Number1 * Number2;
+        break;
+
+        case "division":
+        document.getElementById("result").value = Number1 / Number2;
+        break;
+    }
+}
